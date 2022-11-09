f6e218d9417903cbcb67e1f6d059a1dae1dfc7ac
Katsu <114673266+Katsu8998@users.noreply.github.com>
2022-11-09 23:42:17 +0900

    Update Calculation_Priority ;.java

--- Calculation_Priority ;.java
+++ Calculation_Priority ;.java
@@ -1,15 +1,15 @@
 package Calculation;
 
+import java.util.Scanner;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("値を入力してください");
-		System.out.println("演算子: +, -, *, /,%(除算・余り),^(べき乗),r(平方根)");
+		System.out.println("演算子: +, -, x, ÷,%(除算・余り),^(べき乗),r(平方根)");
 
 		while (true) {
 			// 条件設定
@@ -25,24 +25,41 @@ public class Main {
 				System.err.println("エラー");
 				continue;
 			}
-			//使用可能な演算子
+			// 演算子
 			String[] ops = { "+", "-", "*", "/", "%", "^", "r" };
 
-			// 演算子が1つの場合の計算処理
+			// 計算処理
 			if (!(user_input[1].equals(ops[6])) && user_input[3].equals("")) {
 				int calc;
 				int input1 = Integer.parseInt(user_input[0]);
 				int input2 = Integer.parseInt(user_input[2]);
 				int ans = calcInteger(input1, input2, user_input);
-				System.out.println(ans);
-			} //rの場合の処理
-			else if (user_input[1].equals(ops[6]) && user_input[3].equals("")) {
+				
+				System.out.println("--------ここまでの入力-----------");
+				for (int i = 0; i < user_input.length; i++) {
+					System.out.print(user_input[i]);
+				}
+				System.out.println("");
+				System.out.println("-----------------------------");
+				
+				System.out.println("計算結果 " + ans);
+				continue;
+			} else if (user_input[1].equals(ops[6])&&user_input[3].equals("")) {
 				double calc2;
 				double input3 = Double.parseDouble(user_input[0]);
 				double result = calc2(input3, user_input);
-				System.out.println(result);
+				
+				System.out.println("--------ここまでの入力-----------");
+				for (int i = 0; i < user_input.length; i++) {
+					System.out.print(user_input[i]);
+				}
+				System.out.println("");
+				System.out.println("-----------------------------");
+				
+				System.out.println("計算結果 " +result);
+				continue;
 			}
-			//参照渡しにより、入力可能な値を増やす
+			
 			else if (!(user_input[3].equals(""))) {
 				user_input = tempArray(user_input);
 			}
@@ -59,7 +76,7 @@ public class Main {
 			System.out.println("");
 			System.out.println("-----------------------------");
 
-			// 戻り値を使用した場合の計算処理
+			// 出力
 			int calcInteger;
 			int input1 = Integer.parseInt(user_input[0]);
 			int input2 = Integer.parseInt(user_input[2]);
@@ -68,24 +85,20 @@ public class Main {
 			int middleAns = calcInteger(input1, input2, user_input);
 			int finalAns = calc4(input4, middleAns, user_input);
 
-			//配列1が+,-ではない場合、前から処理を行う
 			if (user_input[1].equals(ops[2]) || user_input[1].equals(ops[3]) || user_input[1].equals(ops[4])
 					|| (user_input[1].equals(ops[5]) || (user_input[1].equals(ops[6])))) {
-				//出力
 				System.out.println(finalAns);
 			}
 
-			// 演算子に優先順位をつけ、計算処理を行う
-			//×,÷,%,^,+は+,-より先に計算を行う
+			// 出力
 			int calc;
 			int ans;
 			int interAns;
 			Deque<String> q = new ArrayDeque<String>();
 
 			// String[] ops = { "+", "-", "*", "/", "%", "^", "r" };
-
-			//初めの演算子が+
-			//次の演算子が*の場合の処理
+			// +,- がarray[1]、×,÷,%,^,+,-がarray[3]にある場合、FILOで処理
+			// 初めの演算子が+、次の演算子が*の場合の処理
 			if ((user_input[1].equals(ops[0]) && user_input[3].equals(ops[2]))) {
 
 				q.addLast(user_input[0]);
@@ -241,7 +254,7 @@ public class Main {
 				}
 			}
 
-			// 初めの演算子が-の場合の処理
+			// 初めの演算子が"-"の場合の処理
 			// 初めの演算子が-, 次の演算子が*の場合の処理
 			if (user_input[1].equals(ops[1]) && user_input[3].equals(ops[2])) {
 
@@ -400,7 +413,7 @@ public class Main {
 
 	}
 
-	// 要素3以下の場合の処理
+	// 要素4以下の場合の処理
 	// 整数計算処理
 	public static int calcInteger(int input1, int input2, String[] user_input) {
 		if (user_input[1].equals("+")) {
@@ -444,7 +457,7 @@ public class Main {
 		return user_input;
 	}
 
-	// 要素4以上、、前から処理を行う場合の計算処理
+	// 要素5以上の場合の計算処理
 	public static int calc4(int input4, int middleAns, String[] user_input) {
 		if (user_input[3].equals("+")) {
 			return middleAns + input4;
